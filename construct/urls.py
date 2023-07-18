6e180f326be6b0b3164308065ea68f971a6545f5
Danilo Maldonado <dannsanto@gmail.com>
2023-07-17 22:16:53 -0300

    modulo estoque envio imagens

--- construct/urls.py
+++ construct/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('usuarios.urls'))
+    path('auth/', include('usuarios.urls')),
+    path('estoque/', include('estoque.urls'))
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- estoque/views.py
+++ estoque/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from .models import Categoria, Produto, Imagem
+from django.http import HttpResponse
+from PIL import Image, ImageDraw
+from datetime import date
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 
-# Create your views here.
+
+def add_produto(request):
+    if request.method == "GET":
+        categorias = Categoria.objects.all()
+        return render(request, 'add_produto.html', {'categorias': categorias})
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        categoria = request.POST.get('categoria')
+        quantidade = request.POST.get('quantidade')
+        preco_compra = request.POST.get('preco_compra')
+        preco_venda = request.POST.get('preco_venda')
+
+        produto = Produto(nome=nome,
+                          categoria_id=categoria,
+                          quantidade=quantidade,
+                          preco_compra=preco_compra,
+                          preco_venda=preco_venda)
+
+        produto.save()
+
+        # Jeito de fazer sem tratamento das imagens
+        # for f in request.FILES.getlist('imagens'):
+        #     img = Imagem(imagem=f, produto=produto)
+        #     img.save()
+
+        for f in request.FILES.getlist('imagens'):
+            name = f'{date.today()}-{produto.id}.jpg'
+
+            img = Image.open(f)
+            img = img.convert('RGB')  # convert o padrão de cor das imagens
+            # redimensiona a imagem para o tamanho definido
+            img = img.resize((800, 600))
+            draw = ImageDraw.Draw(img)
+            # coloca marca dagua na imagem, sendo: posição, texto, data se quiser, e cor em rgb
+            draw.text((20, 580), f"CONSTRUCT {date.today()}", (255, 255, 255))
+            output = BytesIO()  # converte a imagem em bytes
+            # salva a imagem no caminho (output), com o formato e qualidade definidos
+            img.save(output, format="JPEG", quality=100)
+            output.seek(0)
+            img_final = InMemoryUploadedFile(
+                output, 'ImageField', name, 'image/jpeg', sys.getsizeof(output), None)  # converte os bytes do output em imagem novamente
+
+            img_dj = Imagem(imagem=img_final, produto=produto)
+            img_dj.save()
+        return HttpResponse('Foi')
